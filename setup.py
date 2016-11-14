2f0ae0b32c1581f85dbab92d0f820fc589ba7859
Trygve Aspenes aka msg_xrus <Trygve.Aspenes@met.no>
2016-11-14 12:23:33 +0000

    fixed conflict when adding scp ad option

--- setup.py
+++ setup.py
@@ -48,5 +48,6 @@ setup(name="pytroll-file-utils",
       zip_safe=False,
       install_requires=['pyinotify', 'posttroll',
                         'trollsift', 'netifaces',
-                        'pyzmq'],
+                        'pyzmq', 
+                        'scp'],
       )
