133e252699dcdd805c8431ba1cd451ab7de5876d
Lars Orum Rasmussen <ras@dmi.dk>
2017-10-11 07:41:04 +0000

    Added 'name' to logging format

--- bin/move_it_client.py
+++ bin/move_it_client.py
@@ -121,7 +121,7 @@ if __name__ == '__main__':
                         help="Save stats to this file")
     cmd_args = parser.parse_args()
 
-    log_format = "[%(asctime)s %(levelname)-8s] %(message)s"
+    log_format = "[%(asctime)s %(levelname)-8s %(name)s] %(message)s"
     LOGGER = logging.getLogger('')
     LOGGER.setLevel(logging.DEBUG)
 
