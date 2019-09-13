2f9452758b339eeb1579a44852fd27a64bd03988
Martin Raspaud <martin.raspaud@smhi.se>
2019-09-13 17:14:58 +0200

    Fine tune dispatcher verbosity option

--- bin/dispatcher.py
+++ bin/dispatcher.py
@@ -28,19 +28,25 @@ configuration file can be triggered with a `kill -10 <dispatcher pid>`.
 
 import argparse
 import logging
+import logging.handlers
 import os
 
 from trollmoves.dispatcher import Dispatcher
 
 LOG_FORMAT = "[%(asctime)s %(levelname)-8s] %(message)s"
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('dispatcher')
+
+log_levels = {
+    0: logging.WARN,
+    1: logging.INFO,
+    2: logging.DEBUG,
+}
 
 
 def setup_logging(cmd_args):
     """Set up logging."""
-    global logger
-    logger = logging.getLogger('dispatcher')
-    logger.setLevel(logging.DEBUG)
+    root = logging.getLogger('')
+    root.setLevel(log_levels[cmd_args.verbosity])
 
     if cmd_args.log:
         fh_ = logging.handlers.TimedRotatingFileHandler(
@@ -53,7 +59,7 @@ def setup_logging(cmd_args):
     formatter = logging.Formatter(LOG_FORMAT)
     fh_.setFormatter(formatter)
 
-    logger.addHandler(fh_)
+    root.addHandler(fh_)
 
 
 def main():
@@ -63,10 +69,13 @@ def main():
                         help="The configuration file to run on.")
     parser.add_argument("-l", "--log",
                         help="The file to log to. stdout otherwise.")
+    parser.add_argument("-v", "--verbose", dest="verbosity", action="count", default=0,
+                        help="Verbosity (between 1 and 2 occurrences with more leading to more "
+                        "verbose logging). WARN=0, INFO=1, "
+                        "DEBUG=2")
     cmd_args = parser.parse_args()
 
     setup_logging(cmd_args)
-
     logger.info("Starting up.")
 
     try:
