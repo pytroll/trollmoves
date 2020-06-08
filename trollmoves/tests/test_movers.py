8f2e256f62250746ff02ac65950b806816c0c119
Adam.Dybbroe <a000680@c21856.ad.smhi.se>
2020-06-08 14:06:00 +0200

    Add unittest testing login with user and psasword from netrc file
    Signed-off-by: Adam.Dybbroe <a000680@c21856.ad.smhi.se>

---
+++ trollmoves/tests/test_movers.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2020 Adam.Dybbroe
+
+# Author(s):
+
+#   Adam.Dybbroe <a000680@c21856.ad.smhi.se>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Test the movers
+"""
+
+import os
+from tempfile import NamedTemporaryFile, gettempdir
+from unittest.mock import Mock, patch
+
+
+@patch('netrc.netrc')
+def test_open_connection(netrc):
+    """Check getting the netrc authentication."""
+    from trollmoves.movers import FtpMover
+
+    username = 'myusername'
+    password = 'mypasswd'
+    account = None
+
+    origin = '/path/to/mydata/filename.ext'
+
+    def raise_my_error():
+        raise FileNotFoundError
+
+    netrc.side_effect = raise_my_error
+
+    with patch('trollmoves.movers.FTP') as mymock:
+        destination = 'ftp://localhost.smhi.se/data/satellite/archive/'
+        ftp_mover = FtpMover(origin, destination)
+        ftp_mover.open_connection()
+
+        mymock.return_value.login.assert_called_once_with()
+
+    netrc.return_value.hosts = {'localhost.smhi.se': ('myusername', None, 'mypasswd')}
+    netrc.return_value.authenticators.return_value = (username, account, password)
+    netrc.side_effect = None
+
+    with patch('trollmoves.movers.FTP') as mymock:
+        destination = 'ftp://localhost.smhi.se/data/satellite/archive/'
+        ftp_mover = FtpMover(origin, destination)
+        ftp_mover.open_connection()
+
+        mymock.return_value.login.assert_called_once_with(username, password)
+
+    with patch('trollmoves.movers.FTP') as mymock:
+        destination = 'ftp://auser:apasswd@localhost.smhi.se/data/satellite/archive/'
+        ftp_mover = FtpMover(origin, destination)
+        ftp_mover.open_connection()
+
+        mymock.return_value.login.assert_called_once_with('auser', 'apasswd')
