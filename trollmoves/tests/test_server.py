0b9eb642ead978a160f08f5e5ded890d5251cc94
Trygve Aspenes <trygveas@met.no>
2022-11-23 13:32:12 +0100

    update test

--- trollmoves/tests/test_server.py
+++ trollmoves/tests/test_server.py
@@ -232,13 +232,14 @@ class TestMoveItServer:
 @patch("trollmoves.server.RequestManager._set_out_socket")
 @patch("trollmoves.server.RequestManager._get_address_and_payload")
 @patch("trollmoves.server.RequestManager._validate_file_pattern")
-def test_requestmanager_run_valid_pytroll_message(patch_validate_file_pattern,
+@patch("trollmoves.server.RequestManager._process_request")
+def test_requestmanager_run_valid_pytroll_message(patch_process_request,
+                                                  patch_validate_file_pattern,
                                                   patch_get_address_and_payload,
                                                   patch_set_out_socket,
                                                   patch_set_station,
                                                   patch_poller,
-                                                  patch_get_context,
-                                                  caplog):
+                                                  patch_get_context):
     """Test request manager run with valid address and payload."""
     from zmq import POLLIN
     from trollmoves.server import RequestManager
@@ -253,7 +254,7 @@ def test_requestmanager_run_valid_pytroll_message(patch_validate_file_pattern,
     req_man = RequestManager(port)
     req_man.out_socket = 'POLLIN'
     req_man._run_loop()
-    assert caplog.text in ''
+    patch_process_request.assert_called_once()
 
 
 @patch("trollmoves.server.get_context")
@@ -272,10 +273,12 @@ def test_requestmanager_run_MessageError_exception(patch_validate_file_pattern,
     """Test request manager run with invalid payload causing a MessageError exception."""
     from zmq import POLLIN
     from trollmoves.server import RequestManager
+    import logging
     patch_get_address_and_payload.return_value = "address", "fake_payload"
     port = 9876
     patch_poller.return_value = {'POLLIN': POLLIN}
     req_man = RequestManager(port)
     req_man.out_socket = 'POLLIN'
-    req_man._run_loop()
+    with caplog.at_level(logging.DEBUG):
+        req_man._run_loop()
     assert "Failed to create message from payload: fake_payload with address address" in caplog.text
