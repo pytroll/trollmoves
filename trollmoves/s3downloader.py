6b54f72781bb05386d30a80f135aacb182ada12b
Trygve Aspenes <trygveas@met.no>
2022-11-30 09:14:35 +0100

    Fix copyright

--- trollmoves/s3downloader.py
+++ trollmoves/s3downloader.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2022
-#
-# Author(s):
-#
-#   Trygve Aspenes <trygveas@met.no>
+# Copyright (c) 2020 Pytroll developers
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
