c9186384a509d695090423c3393e3a876676ffa6
Martin Raspaud <martin.raspaud@smhi.se>
2024-04-26 11:37:56 +0200

    Mock fsspec for autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@ author = 'Pytroll devs'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc", "sphinx.ext.intersphinx"]
+autodoc_mock_imports = ["fsspec"]
 
 templates_path = ['_templates']
 exclude_patterns = []
